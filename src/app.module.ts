35d0fff2bfd2626c9c30a04f2463d25fedc61bdf
DESKTOP-OQPLL9U\user <georgemunganga@gmail.com>
2025-09-27 20:23:35 +0200

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './modules/auth';
+import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-// import { PrismaModule } from './modules/common/prisma/prisma.module';
-// import { SecurityModule } from './modules/security/security.module';
-// import { CommunicationModule } from './modules/communication/communication.module';
+import { PrismaModule } from './modules/common/prisma/prisma.module';
+import { SecurityModule } from './modules/security/security.module';
+import { CommunicationModule } from './modules/communication/communication.module';
+import { ErrandsModule } from './modules/errands/errands.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // PrismaModule,
-    // Comment out modules that might be causing errors
-    // CommunicationModule,
-    // SecurityModule,
-    //AuthModule,
+    PrismaModule,
+    AuthModule,
     UsersModule,
+    SecurityModule,
+    CommunicationModule,
+    ErrandsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
