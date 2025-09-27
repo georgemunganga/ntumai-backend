b23f81adca8a5680a3de93851376dab12880f3d2
DESKTOP-OQPLL9U\user <georgemunganga@gmail.com>
2025-09-27 23:09:15 +0200

    features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { PrismaModule } from './modules/common/prisma/prisma.module';
-<<<<<<< HEAD
-import { MarketplaceModule } from './modules/marketplace';
-=======
-import { SecurityModule } from './modules/security/security.module';
 import { CommunicationModule } from './modules/communication/communication.module';
+import { ConfigModule } from '@nestjs/config';
 import { ErrandsModule } from './modules/errands/errands.module';
->>>>>>> main
+import { MarketplaceModule } from './modules/marketplace';
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './modules/common/prisma/prisma.module';
+import { SecurityModule } from './modules/security/security.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -21,13 +18,10 @@ import { ErrandsModule } from './modules/errands/errands.module';
     PrismaModule,
     AuthModule,
     UsersModule,
-<<<<<<< HEAD
     MarketplaceModule,
-=======
     SecurityModule,
     CommunicationModule,
     ErrandsModule,
->>>>>>> main
   ],
   controllers: [AppController],
   providers: [AppService],
